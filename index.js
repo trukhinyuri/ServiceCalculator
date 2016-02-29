f1eca0568f3c8a8da8b91cfc802667d60fabe092
trukhinyuri <yuri@trukhin.com>
2016-02-29 15:36:36 +0300

    Added info about Tier III Gold

--- index.js
+++ index.js
@@ -123,7 +123,7 @@
                 if (regionValue == 0) {
                     cellRegion.appendChild(document.createTextNode("Россия, Санкт-Петербург (SSD)"));
                 } else if (regionValue == 1) {
-                    cellRegion.appendChild(document.createTextNode("Россия, Москва (SSD-cache)"));
+                    cellRegion.appendChild(document.createTextNode("Россия, Москва (Tier III Gold, SSD-cache)"));
                 } else if (regionValue == 2) {
                     cellRegion.appendChild(document.createTextNode("Европа, Амстердам (SSD-cache)"));
                 }
